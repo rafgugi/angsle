8d5b288490c697ed186513c7bb9a8b442c591cc9
Risyanggi Azmi Faizin <risyanggi.azmi@bukalapak.com>
2021-06-20 16:08:53 +0700

    implement dummy func body

--- sec/sec.go
+++ sec/sec.go
@@ -3,9 +3,15 @@ package sec
 import "github.com/rafgugi/angsle/entity"
 
 // EncryptUser returns cipher, encryption of user
-func EncryptUser(user entity.User, privateKey, publicKey string) string
+func EncryptUser(user entity.User, privateKey, publicKey string) string {
+	return ""
+}
 
 // DecryptUser returns user, from given cipher text
-func DecryptUser(cipher, publickKey string) entity.User
+func DecryptUser(cipher, publickKey string) entity.User {
+	return entity.User{}
+}
 
-func Sha256(msg string) string
+func Sha256(msg string) string {
+	return ""
+}
